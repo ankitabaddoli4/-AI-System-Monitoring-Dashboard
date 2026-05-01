374c0c7fc5a1aa5679749c1749a2fe43739c24d9
ankitabaddoli4 <ankitabaddoli4@gmail.com>
2026-05-01 21:22:43 +0530

    Update predict.py

--- ml/predict.py
+++ ml/predict.py
@@ -1,8 +1,22 @@
+import joblib
+
+model = None
+
+def load_model():
+    global model
+    if model is None:
+        try:
+            model = joblib.load("ml/model.pkl")
+        except Exception:
+            model = None
+    return model
+
+
 def predict_usage(data):
     model = load_model()
 
     if model:
         result = model.predict([data])
-        return result[0]   # ✅ always return number
+        return result[0]   # always return number
     else:
-        return sum(data) / len(data)
+        return sum(data) / len(data)  # fallback
