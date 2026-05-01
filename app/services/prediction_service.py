61d0bb0a7885ea40f2c2aae4b6bda5baea4aaabe
ankitabaddoli4 <ankitabaddoli4@gmail.com>
2026-05-01 21:23:10 +0530

    Update prediction_service.py

--- app/services/prediction_service.py
+++ app/services/prediction_service.py
@@ -1,12 +1,10 @@
+from ml.predict import predict_usage
+
 def predict_future():
-    data = [10, 20, 30]  # example
-    current = 50         # example
+    data = [10, 20, 30]   # example
+    current = 50          # example
 
     prediction = predict_usage(data)
-
-    # FIX HERE 👇
-    if isinstance(prediction, list):
-        prediction = prediction[0]
 
     prediction = (prediction + current) / 2
 
